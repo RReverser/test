d2a62a55e51067ca24511496488e76a5d98b2f7b
Ingvar Stepanyan <me@rreverser.com>
2014-10-22 13:51:26 +0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
-module.exports = function (grunt) {
-	require('load-grunt-config')(grunt, {
-		data: {
-			pkgName: 'jbinary'
-		}
-	});
-};
+module.exports = function (grunt) {!!!
+	require('load-grunt-config')(grunt, {!!!
+		data: {!!!
+			pkgName: 'jbinary'!!!
+		}!!!
+	});!!!
+};!!!
